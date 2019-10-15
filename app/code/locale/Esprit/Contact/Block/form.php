b09a5a77b44c41046dffaf6c49c743772a336476
Maalej Mohamed Badis <35582138+Badis1337@users.noreply.github.com>
2019-10-15 10:46:58 +0100

    Initial commit

---
+++ app/code/locale/Esprit/Contact/Block/form.php
@@ -0,0 +1,9 @@
+<?php
+class Esprit_Contact_Block_Form extends Mage_Core_Block_Template{
+
+public function getName(){
+    echo "Bonjour Block";
+
+    return "Bonjour";
+}
+}
---
+++ app/code/locale/Esprit/Contact/Controllers/IndexController.php
@@ -0,0 +1,8 @@
+<?php
+
+class Esprit_Contact_IndexController extends Mage_Core_Controller_Front_Action {
+    
+    public function indexAction(){
+        $this->loadLayout()->renderLayout();
+    }
+}
